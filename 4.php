fbd52d196b5994b2c7a4b2ea1fc059970c9e8865
phutisemenya <40797050+phutisemenya@users.noreply.github.com>
2018-07-16 15:41:56 +0200

    create High-Entropy Passphrases

---
+++ 4.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * High-Entropy Passphrases 
+ */
+class PassPhrase
+{
+	public function validatePass($passphrase = "")
+	{
+		$message = $passphrase." is valid \n";
+		if(!empty($passphrase))
+		{
+			$pass = strtolower($passphrase);	// convert string to lower-case
+			$strArr = explode(" ",$pass);		// Convert string to array
+			$data = array();
+			
+			foreach ($strArr as $key => $item)
+			{
+				$str = trim($item);		// Remove whitespacies
+				
+				if (isset($data[$str]))	// Check if 
+				{
+					$data[$str] += 1;
+				} else {
+					$data[$str] = 1;
+				}
+				
+				if ($data[$str] > 1)
+				{
+					$message = $passphrase." is not valid \n";
+					break;
+				}
+			}
+						
+		}
+		return $message;
+	}
+}
+
+$PassPhrase = new PassPhrase();
+print $PassPhrase->validatePass("aa bb cc dd ee");
+print $PassPhrase->validatePass("aa bb cc dd aa");
+print $PassPhrase->validatePass("aa bb cc dd aaa");
+print $PassPhrase->validatePass("bdwdjjo avricm cjbmj ran lmfsom ivsof");
+?>
